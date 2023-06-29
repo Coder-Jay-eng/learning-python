0618d17bf753717546c783d39797941c0ea9b814
Aw3som3ss <jabwunza@gmail.com>
2023-06-29 15:10:17 +0300

    Getting input from Users

---
+++ user-input.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+# prompts the user to input their name
+
+age = input("Enter your age: ")
+# prompts the user to input their age
+
+print("Hello " + name + "! You are " + age + " years old.")
+# prints out 'Hello "Username"! You are "Userage" years old.
