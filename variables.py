b26fb744531c6115e0b34eeeae81488f4e181806
Aw3som3ss <jabwunza@gmail.com>
2023-06-29 13:54:23 +0300

    Variables and Data Types

---
+++ variables.py
@@ -0,0 +1,8 @@
+char_name = "John"
+char_age = "23"
+print("There once was a man named " + char_name + ",")
+print("he was " + char_age + " years old.")
+# updated variable
+char_name = "Jay"
+print("He really liked the name " + char_name + ",")
+print("But didn't like being " + char_age + " yrs old.")
