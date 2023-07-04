4c3a3bf6bbc3ed242f14286d3931fa2f746aa54b
Aw3som3ss <jabwunza@gmail.com>
2023-07-04 14:27:52 +0300

    Return statement

---
+++ cube.py
@@ -0,0 +1,9 @@
+def cube(num):
+    return num * num * num
+
+
+# return value allows getting information from a function to the called function. It is also the last piece of code in a function.
+
+result = cube()
+
+print(result)
