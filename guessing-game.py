8b2f8b13aff02d9cea3c9a20a107ed36f809fa2d
Aw3som3ss <jabwunza@gmail.com>
2023-07-05 16:03:33 +0300

    The guessing game with a clue

--- guessing-game.py
+++ guessing-game.py
@@ -3,6 +3,9 @@ guess = ""
 guess_count = 0
 guess_limit = 5
 out_of_guesses = False
+clue = "Has a long neck."
+
+print(clue)
 
 while guess != secret_word and not (out_of_guesses):
     if guess_count < guess_limit:
