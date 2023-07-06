120991621f72df1817b7ff4e65a6257865feefc4
Aw3som3ss <jabwunza@gmail.com>
2023-07-06 15:58:59 +0300

    Building a simple translator

---
+++ basic-translator.py
@@ -0,0 +1,16 @@
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        
+        if letter.lower() in "aeiou":
+            #if letter in "AEIOUaeiou":
+            if letter.isupper():
+            translation = translation + "J"
+            else:
+            translation = translation + "j"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
