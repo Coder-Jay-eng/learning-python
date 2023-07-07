6d27adad1dd9e25caf9484b5974ff12545392752
Aw3som3ss <jabwunza@gmail.com>
2023-07-07 13:05:53 +0300

    classes and objects in python

---
+++ classes.py
@@ -0,0 +1,11 @@
+# classes help make programmes more organised and powerful
+
+from student import Student
+
+
+student1 = Student("Jay", "IT", 3.7, False)
+student2 = Student("Jade", "BS", 2.9, True)
+student3 = Student("Becky", "IR", 3.0, False)
+student4 = Student("Pam", "BA", 3.9, False)
+
+print(student3.major)
